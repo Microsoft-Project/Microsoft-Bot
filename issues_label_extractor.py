a82f1d62f6e6b4fc655d0d1607838ea243924e8e
totsteps <totsteps.gs@gmail.com>
2020-09-14 04:05:48 +0200

    Refactor files

--- issues_label_extractor.py
+++ issues_label_extractor.py
@@ -1,53 +1,61 @@
-import pprint
 import numpy as np
 from pymongo import MongoClient
-from PyDictionary import PyDictionary
 
 client = MongoClient('mongodb://localhost:27017')
 db = client['github']
 
 
-class issueLabels:
-
-    def extractlabels(self,collectionList):
-        if 'Trend_repo_names' and 'issues' in collectionList:
-            query = db.get_collection('Trend_repo_names').aggregate([
-                {'$lookup':
+class IssueLabel:
+    @staticmethod
+    def get_labels(collection):
+        if 'trending_repo_names' and 'issues' in collection:
+            trending_repo_names = db.get_collection(
+                'trending_repo_names'
+            ).aggregate(
+                [
                     {
-                        'from': 'issues',
-                        'localField': 'name',
-                        'foreignField': 'repo',
-                        'as': 'array'
-                    }
-                },
-                {'$unwind': '$array'},
-                {'$project': {'array.labels': True, 'name': True, '_id': False}}
-            ])
+                        '$lookup': {
+                            'from': 'issues',
+                            'localField': 'name',
+                            'foreignField': 'repo',
+                            'as': 'array',
+                        },
+                    },
+                    {'$unwind': '$array'},
+                    {
+                        '$project': {
+                            'array.labels': True,
+                            'name': True,
+                            '_id': False,
+                        }
+                    },
+                ],
+            )
 
+            issue_labels = []
+            repo_companies = []
 
-            issuesList = []
-            company =  []
-            count = 0
-            for list_pull_requests in query:
-                if len(list_pull_requests['array']['labels']) > 0:
-                    company.append(list_pull_requests['name'])
-                    issuesList.append(list_pull_requests['array']['labels'])
+            for pull_requests in trending_repo_names:
+                if len(pull_requests['array']['labels']) > 0:
+                    issue_labels.append(pull_requests['array']['labels'])
+                    repo_companies.append(pull_requests['name'])
 
-
-            newComp = []
-            i = 0
+            # TODO: See if we can get rid of these variables
+            #   Why don't we extract the names directly?
+            new_repo_companies = []
             labels = []
-            for lists in issuesList:
-                for obj in lists:
-                    labels.append(obj['name'])
-                    newComp.append(company[i])
+            i = 0
+            for lists in issue_labels:
+                for list_item in lists:
+                    labels.append(list_item['name'])
+                    new_repo_companies.append(repo_companies[i])
                 i += 1
 
-            numArray = np.array([])
-            for i in range(len(company)):
-                temp = np.array([[newComp[i], labels[i]]])
-                numArray = np.append(numArray, temp)
+            num_array = np.array([])
+            for i in range(len(repo_companies)):
+                temp = np.array([[new_repo_companies[i], labels[i]]])
+                num_array = np.append(num_array, temp)
 
-            numArray = numArray.reshape(int((numArray.size / 2)), 2)
+            num_array = num_array.reshape(int((num_array.size / 2)), 2)
 
-            return numArray
+            return num_array
