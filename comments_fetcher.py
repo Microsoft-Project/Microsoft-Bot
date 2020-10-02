62ce0e99ac55d316191f067615b0fa251d63cc37
Gagandeep Singh <totsteps.gs@gmail.com>
2020-10-02 20:38:32 +0200

    Extend to using default Github labels

--- comments_fetcher.py
+++ comments_fetcher.py
@@ -1,34 +1,56 @@
-import enchant
 import requests
 
-dictionary = enchant.Dict("en_US")
+
+def fetch_issue_data(repo_full_name):
+    bug = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=bug')
+    doc = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=documentation')
+    wont_fix = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=wontfix')
+    duplicate = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=duplicate')
+    enhancement = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=enhancement')
+    good_first_issue = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=good%20first%20issue')
+    help_wanted = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=help%20wanted')
+    invalid = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=invalid')
+    question = requests.get(f'https://api.github.com/repos/{repo_full_name}/issues?labels=question')
+
+    bug_list = get_data_from_json(bug)
+    doc_list = get_data_from_json(doc)
+    wont_fix_list = get_data_from_json(wont_fix)
+    duplicate_list = get_data_from_json(duplicate)
+    enhancement_list = get_data_from_json(enhancement)
+    good_first_issue_list = get_data_from_json(good_first_issue)
+    help_wanted_list = get_data_from_json(help_wanted)
+    invalid_list = get_data_from_json(invalid)
+    question_list = get_data_from_json(question)
+
+    hash_map = dict({
+        'bug': bug_list,
+        'documentation': doc_list,
+        'wont_fix': wont_fix_list,
+        'duplicate': duplicate_list,
+        'enhancement': enhancement_list,
+        'good_first_issue': good_first_issue_list,
+        'help_wanted': help_wanted_list,
+        'invalid': invalid_list,
+        'question': question_list
+    })
+
+    return hash_map
 
 
-def fetch():
-    bug = requests.get('https://api.github.com/repos/opencv/opencv/issues?labels=bug')
-    doc = requests.get('https://api.github.com/repos/opencv/opencv/issues?labels=documentation')
-    wontfix = requests.get('https://api.github.com/repos/opencv/opencv/issues?labels=wontfix')
+def get_data_from_json(json_data):
+    """
+    Returns list containing only 'body' from issue comments.
 
-    data_bug = bug.json()
-    data_doc = doc.json()
-    data_wontfix = wontfix.json()
+    :param json_data: json input from Github API
+    :return: list
+    """
+    tmp_arr = []
 
-    bug_list = []
-    doc_list = []
-    wontfix_list = []
+    for i in json_data.json():
+        tmp_arr.append(i['body'])
 
-    for i in data_bug:
-        bug_list.append(i['body'])
+    return tmp_arr
 
-    for j in data_doc:
-        doc_list.append(j['body'])
 
-    for k in data_wontfix:
-        wontfix_list.append(k['body'])
-
-    map = dict({'bug': bug_list, 'documentation': doc_list, 'wontfix': wontfix_list})
-
-    return map
-
-# print(len(bug_list), len(doc_list), len(wontfix_list))
-# print(map)
+# this is just for example
+# print(fetch_issue_data('nanoninja/docker-nginx-php-mysql'))
